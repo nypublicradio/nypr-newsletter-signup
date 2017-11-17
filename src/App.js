683075ad2b6de92073da98c663d1c2caf14c4bbf
Brian Whitton <me@brianwhitton.com>
2017-11-17 16:03:02 -0500

    initial styles and broken tests

--- src/App.js
+++ src/App.js
@@ -1,20 +1,79 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
+import SubscribeForm from 'react-mailchimp-subscribe';
 import './App.css';
+import './SignupForm.css';
+import pym from 'pym.js';
+
+const ACTION = 'https://nypublicradio.us5.list-manage.com/subscribe/post?u=4109fdd323aaac7078eadaa8f';
+
+const FORM_PROPS = {
+  messages: {
+    inputPlaceholder: 'Enter your email',
+    btnLabel: 'Sign Up',
+    sending: 'Sending...',
+    success: 'Thanks for signing up!',
+    error: 'Looks like there was an error.'
+  },
+  styles: {
+    sending: {
+      fontSize: 18,
+      color: "auto"
+    },
+    success: {
+      fontSize: 18,
+      color: "green"
+    },
+    error: {
+      fontSize: 18,
+      color: "red"
+    }
+  },
+  className: 'SignupForm'
+}
 
 class App extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      mailchimpId: this.props.mailchimpId
+    };
+    
+    new pym.Child({polling: 200});
+  }
+  
+  componentDidMount() {
+    window.addEventListener('message', ({data, origin}) => {
+      if (origin !== process.env.REACT_APP_TOOLKIT_ORIGIN && process.env.NODE_ENV !==  'test') {
+        return;
+      } else {
+        this.listener(data);
+      }
+    })
+  }
   render() {
+    FORM_PROPS.action = ACTION + `&id=${this.state.mailchimpId}`;
     return (
       <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h1 className="App-title">Welcome to React</h1>
-        </header>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
+        <h1 className="App__headline">Stay up-to-date</h1>
+        
+        <SubscribeForm {...FORM_PROPS} />
       </div>
     );
+  }
+  
+  parse(data) {
+    let message = {};
+    if (typeof data === 'string') {
+      try {
+        message = JSON.parse(data);
+      } catch(e) {/* Ignored */}
+    }
+    return message;
+  }
+  
+  listener(data) {
+    let { query } = this.parse(data);
+    this.setState(query);
   }
 }
 
--- src/App.test.js
+++ src/App.test.js
@@ -1,8 +1,45 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import App from './App';
+import { shallow } from 'enzyme';
 
 it('renders without crashing', () => {
   const div = document.createElement('div');
   ReactDOM.render(<App />, div);
 });
+
+it('renders query params', () => {
+  const props = {
+    headline: 'foo',
+    summary: 'bar',
+    callToAction: 'baz',
+    url: 'http://buz.com'
+  };
+  const app = shallow(<App {...props} />);
+  
+  expect(app.find('.App__headline').text()).toEqual(props.headline);
+  expect(app.find('.App__summary').text()).toEqual(props.summary);
+  expect(app.find('.App__call-out').text()).toEqual(props.callToAction);
+  expect(app.find('.App__call-out').prop('href')).toEqual(props.url);
+});
+
+it('responds to postMessage', done => {
+  const props = {
+    headline: 'foo',
+    summary: 'bar',
+    callToAction: 'baz',
+    url: 'http://buz.com'
+  };
+  const app = shallow(<App />);
+  jest.spyOn(app.instance(), 'listener').mockImplementation(data => {
+    expect(data).toEqual(JSON.stringify(props));
+    done();
+  });
+  
+  window.postMessage(JSON.stringify(props), '*');
+});
+
+it('shows a preview message', () => {
+  const app = shallow(<App />);
+  expect(app.find('.App__placeholder').text()).toEqual('Fill out the fields and your preview will appear here');
+})
