6b06000d4b71304e8cf1ee9c40a1cbc11878d516
Brian Whitton <me@brianwhitton.com>
2017-11-20 13:16:38 -0500

    adds signup form test

---
+++ src/SignupForm.test.js
@@ -0,0 +1,31 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import SignupForm from './SignupForm';
+import { mount } from 'enzyme';
+
+it('renders without crashing', () => {
+  const REQUIRED_PROPS = {
+    messages: {},
+    action: '',
+    styles: {}
+  };
+  const div = document.createElement('div');
+  ReactDOM.render(<SignupForm {...REQUIRED_PROPS} />, div);
+});
+
+it('it passes the url to the submit method', done => {
+  const PROPS = {
+    action: 'http://mailchimp.com',
+    messages: {},
+    styles: {}
+  };
+  const EMAIL = 'foo@bar.com';
+  const form = mount(<SignupForm {...PROPS} />);
+  jest.spyOn(form.instance(), 'submit').mockImplementation(url => {
+    expect(url).toEqual(PROPS.action + `&EMAIL=${encodeURIComponent(EMAIL)}`);
+    done();
+  });
+  
+  form.find('input').instance().value = EMAIL;
+  form.find('button').simulate('click');
+})
