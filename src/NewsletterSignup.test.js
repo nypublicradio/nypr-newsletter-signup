cc9351586048e050bc0eb52628b884661b53e4c8
Brian Whitton <me@brianwhitton.com>
2018-02-02 11:46:20 -0500

    update imports

--- src/NewsletterSignup.test.js
+++ src/NewsletterSignup.test.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import App from './App';
+import NewsletterSignup from './NewsletterSignup';
 import { mount } from 'enzyme';
 import pym from 'pym.js';
 
--- src/index.js
+++ src/index.js
@@ -1,11 +1,11 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
-import App from './App';
+import NewsletterSignup from './NewsletterSignup';
 import queryString from 'query-string';
 import pym from 'pym.js';
 
 const params = queryString.parse(window.location.search);
 const embed = new pym.Child({polling: 200});
 
-ReactDOM.render(<App {...params} embed={embed} />, document.getElementById('root'));
+ReactDOM.render(<NewsletterSignup {...params} embed={embed} />, document.getElementById('root'));
