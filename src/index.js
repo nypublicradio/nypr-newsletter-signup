1bc3f011eaba581fdc447d3355e990b9320db75d
Brian Whitton <me@brianwhitton.com>
2019-03-11 16:15:33 -0400

    base class handles this

--- src/index.js
+++ src/index.js
@@ -2,10 +2,5 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
 import NewsletterSignup from './NewsletterSignup';
-import queryString from 'query-string';
-import pym from 'pym.js';
 
-const params = queryString.parse(window.location.search);
-const embed = new pym.Child({polling: 200});
-
-ReactDOM.render(<NewsletterSignup {...params} embed={embed} />, document.getElementById('root'));
+ReactDOM.render(<NewsletterSignup />, document.getElementById('root'));
